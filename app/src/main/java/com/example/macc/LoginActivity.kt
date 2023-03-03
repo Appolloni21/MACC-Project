bb768b94af4a60c5f2a77252dc78098dfd309e28
Appolloni21 <alessandro.appolloni21@gmail.com>
2023-03-03 18:49:58 +0100

    login and logout with firebase

---
+++ app/src/main/java/com/example/macc/LoginActivity.kt
@@ -0,0 +1,91 @@
+package com.example.macc
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+
+class LoginActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+
+
+        val passwordEditText = findViewById<EditText>(R.id.password_text)
+
+        val loginButton: Button = findViewById(R.id.login_btn)
+        loginButton.setOnClickListener {
+            when {
+                TextUtils.isEmpty(
+                    findViewById<EditText>(R.id.email_text).text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(
+                    findViewById<EditText>(R.id.password_text).text.toString()
+                        .trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                else -> {
+
+
+                    val email: String =
+                        findViewById<EditText>(R.id.email_text).text.toString().trim { it <= ' ' }
+                    val password: String =
+                        passwordEditText.text.toString().trim { it <= ' ' }
+
+                    //Login user with email and password
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener{ task ->
+                            //If the login is successfully done
+                            if (task.isSuccessful) {
+
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "You are logged in successfully",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                //User is logged in, we send him to the homepage
+                                val intent = Intent(
+                                    this@LoginActivity,
+                                    MainActivity::class.java
+                                )
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                //If the login was not successful, then show the error message
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+
+                        }
+                }
+            }
+        }
+    }
+
+}
+
--- app/src/main/java/com/example/macc/MainActivity.kt
+++ app/src/main/java/com/example/macc/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.macc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.macc.adapter.ItemAdapter
 import com.example.macc.data.Datasource
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide();
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.user1_personal_page)
+        setContentView(R.layout.user_my_profile_page)
 
         //TEST PER LA HOMEPAGE
         // Initialize data.
@@ -23,6 +28,22 @@ class MainActivity : AppCompatActivity() {
         // Use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         //recyclerView.setHasFixedSize(true)
+
+
+        //Da spostare poi nel fragment della user_my_profile_page
+        //Handle logout
+        val logoutButton: Button = findViewById(R.id.logout_btn)
+        logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            Toast.makeText(
+                this@MainActivity,
+                "You are now logged out",
+                Toast.LENGTH_SHORT
+            ).show()
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            this@MainActivity.finish()
+            //true
+        }
 
     }
 }
