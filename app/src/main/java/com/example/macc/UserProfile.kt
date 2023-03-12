79522b23981abd6de0d114b625a7c82cd095f7c9
Appolloni21 <alessandro.appolloni21@gmail.com>
2023-03-12 12:36:02 +0100

    homepage visualization improvements and other fix

--- app/src/main/java/com/example/macc/UserProfile.kt
+++ app/src/main/java/com/example/macc/UserProfile.kt
@@ -1,24 +1,25 @@
 package com.example.macc
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 
-class UsersList : Fragment() {
+
+class UserProfile : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        val view: View = inflater.inflate(R.layout.users_list, container,
+        val view: View = inflater.inflate(R.layout.user_profile_page, container,
             false)
         return view
     }
@@ -31,10 +32,6 @@ class UsersList : Fragment() {
         view.findViewById<Toolbar>(R.id.toolbar)
             .setupWithNavController(navController, appBarConfiguration)
 
-        val userProfileImage = view.findViewById<ImageView>(R.id.profile_image)
-        userProfileImage.setOnClickListener{
-            val action = UsersListDirections.actionUsersListToUserProfile()
-            view.findNavController().navigate(action)
-        }
     }
+
 }
