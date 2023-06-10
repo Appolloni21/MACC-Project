0b609329aaca3842cf5c2b8fa834f97a5acf11d3
Pietro Lamanna <89349573+plamax1@users.noreply.github.com>
2023-06-10 19:52:20 +0200

    first firebase integration

--- server.py
+++ server.py
@@ -1,6 +1,18 @@
 # server.py
 
 from flask import Flask, jsonify, request
+from firebase_admin import db
+import firebase_admin
+
+cred_obj = firebase_admin.credentials.Certificate('serviceAccountKey.json')
+print(cred_obj)
+databaseURL = 'https://macc-project-1acfb-default-rtdb.europe-west1.firebasedatabase.app/'
+default_app = firebase_admin.initialize_app(cred_obj, {
+	'databaseURL':databaseURL
+	})
+ref = db.reference("/users/")
+print(ref.order_by_child("email").get())
+print(ref)
 
 app = Flask(__name__)
 user_id=00000
