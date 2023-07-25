e7db5764ae07e41a6828f2f0ab82d7371cf33d27
nojano <62405268+nojano@users.noreply.github.com>
2023-07-25 13:59:05 +0200

    Insert expense first try

--- app/src/main/java/com/example/macc/InsertExpense.kt
+++ app/src/main/java/com/example/macc/InsertExpense.kt
@@ -1,18 +1,30 @@
 package com.example.macc
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.example.macc.viewmodel.PriceViewModel
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 
-class InsertExpense : Fragment() {
+public class InsertExpense : Fragment() {
+
+     private val viewModel: PriceViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,5 +48,36 @@ class InsertExpense : Fragment() {
             navController.navigateUp()
         }
 
+        /*val takeAPhotoBtn = view.findViewById<MaterialButton>(R.id.button_take_photo)
+        val activityContext = requireActivity()
+        takeAPhotoBtn.setOnClickListener {
+            val intent = Intent(activityContext,TextRecognition::class.java)
+            startActivity(intent)
+        }
+        val priceField = view.findViewById<EditText>(R.id.priceText)
+        val loadThePhotoBtn = view.findViewById<MaterialButton>(R.id.loadPhotoBtn)
+            loadThePhotoBtn.setOnClickListener {
+                var Item = "5"
+                priceField.setText(Item)
+                viewModel.selected.observe(activityContext, Observer<String> { item ->
+                    var Item = "5"
+                    if(item.isNotEmpty()){
+                        priceField.setText(Item)
+                    }
+                    else{
+                        showToast("COGLIONEEEEEEEEEEEEEEEE")
+                    }
+
+                })
+            }
+
+*/
     }
+
+
+    private fun showToast(message: String){
+        val activityContext = requireActivity()
+        Toast.makeText(activityContext, message, Toast.LENGTH_SHORT).show()
+    }
+
 }
